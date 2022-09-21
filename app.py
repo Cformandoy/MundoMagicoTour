61cb29848dfdafb12afb2dff300b8dd179b2407f
cformandoy <cformandoy@gmail.com>
2022-09-21 14:49:19 -0400

    Se prueba el login

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from crypt import methods
 from urllib import request
 from flask import Flask, render_template, request, url_for, redirect
 import eel
-
+import flask_login
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
@@ -10,6 +10,75 @@ from googleapiclient import discovery
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "cp209182793"
+
+
+login_manager = flask_login.LoginManager()
+
+login_manager.init_app(app)
+
+users = {'foo@bar.tld': {'password': 'secret'},}
+
+
+class User(flask_login.UserMixin):
+    pass
+
+
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if email in users and flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized', 401
+
 
 #GOOGLE API
 
